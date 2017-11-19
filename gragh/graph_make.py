13aa756af4d16e4957105638d45a325167ad297b
Yuqing Xia <felizxia@umich.edu>
2017-11-19 00:35:01 -0500

    add concrete functions of write graph and make visualizations
    1) follow_relationship_write(): is function to write follower or following relationship based on your crawl data, output is text file. format is like (user_id user_following/follower)
    
    2) write_graph (): is to write gml file based on the text file.
    
    3) degree_separate() : will create 1000,3000 and 8000 random nodes according to our file and build degree separation graphs. I believe you can change this random nodes setting for our other accounts information. Some are very small, so you can just ignore it and run all the nodes
    
    4) degree_distribution_cdf(): is a function to draw degree distribution based on CDF.

--- gragh/graph_make.py
+++ gragh/graph_make.py
@@ -1,10 +1,41 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+from numpy import cumsum
+import json
+import os
+import fnmatch
+from itertools import chain
 
-# write graph function
+
+def follow_relationship_write(file):
+    following_net = open(str(file)+str('.txt'), 'w')
+    dict = {}
+    for file in os.listdir('/Users/rita/Google Drive/608/network/following/'): # change path
+        root, ext = os.path.splitext(file)
+        if ext == '.json':
+            with open(str('/Users/rita/Google Drive/608/network/following/' + file)) as follow:
+                following = json.load(follow)
+                for i in following:
+                    user_id = i[0]
+                    for j in i:
+                        if type(j) is list:
+                            for k in j:
+                                fol = k.get('id')
+                                dict[user_id] = fol
+                                following_net.write(str(user_id) + '\t' + str(fol))
+                                following_net.write('\n')
+    print (len(list(dict.keys())))
+    print (len(list(dict.values())))
+
+follow_relationship_write('followings')
+follow_relationship_write('followers')
+
+# load followers and following file; write graph function
 def write_graph(file):
     fol = nx.DiGraph()
-
     with open(str(file)+str('.txt'))as follow:
         for line in follow:
             follower = line.strip().split('\t')
@@ -12,16 +43,18 @@ def write_graph(file):
             fol.add_edge(*follower)
     nx.write_gml(fol,str(file) + ".gml")
     return fol
+
 write_graph("user_followings")
 write_graph("user_followers")
 
-## read file
+## read graph file, print basic information
 followers= nx.read_gml("user_followers.gml")
 followings= nx.read_gml("user_followings.gml")
-# print basic info about files
-print (nx.info(followers))
-print (nx.info(followings))
-# # random pick a node, do shortest path length
+# print (nx.info(followers))
+# print (nx.info(followings))
+
+
+# random pick a node, calculate shortest path length
 def degree_separate(file):
     from random import sample
     import json
@@ -33,10 +66,9 @@ def degree_separate(file):
     source_list_one = sample(file.nodes(),sample_n1)
     source_list_two = sample(file.nodes(),sample_n2)
     source_list_three = sample(file.nodes(),sample_n3)
-    sample_1000 = open('sample_on_1000_nodes_following.csv','w')
+    sample_1000 = open('sample_on_1000_nodes_following.csv','w') # you can save it or not
     sample_3000 = open('sample_on_3000_nodes_following.csv','w')
     sample_8000 = open('sample_on_8000_nodes_following.csv','w')
-    # print(source_list)
     counts_path_1=dict()
     counts_path_2=dict()
     counts_path_3=dict()
@@ -63,7 +95,7 @@ def degree_separate(file):
                 path_valuesthree = list(dicthree.values())
                 for item in path_valuesthree:
                     counts_path_3[item] = counts_path_3.get(item, 0) + 1
-    print(counts_path_1)
+    print(counts_path_1) # check path dictionary
     print(counts_path_2)
     print(counts_path_3)
     r1, g1, b1 = np.random.uniform(0, 1, 3)
@@ -84,3 +116,45 @@ def degree_separate(file):
     plt.title('Degree of separation')
     plt.show()
     plt.savefig('degree_of_separation_'+str(file)+'.png')
+
+degree_separate(followers)
+degree_separate(followings)
+# cdf distribution of graphs
+
+def degree_separation_cdf(file):
+    if file =="user_followings":
+        dict_fl = defaultdict(int)
+        for i in list(followings.out_degree().values()):
+            dict_fl[i] += 1
+        items=sorted(dict_fl.items(),reverse=True)
+        x,y = np.array(items).T
+        y = [float(i)/sum(y) for i in y]
+        y = cumsum(y)
+        plt.plot(x, y, 'bo')
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.legend(['in_Degree'])
+        plt.xlabel('$K$', fontsize=20)
+        plt.ylabel('$P_K$', fontsize=20)
+        plt.title('$Degree\,Distribution\,of\,Followers$', fontsize=20)
+        plt.show()
+    if file=="user_followers":
+        dict_fr = defaultdict(int)
+        for i in list(followers.in_degree().values()):
+            dict_fr[i] += 1
+        for i in list(followers.in_degree().values()):
+            dict_fr[i]+=1
+        items=sorted(dict_fr.items(),reverse=True)
+        x,y = np.array(items).T
+        y = [float(i)/sum(y) for i in y]
+        y = cumsum(y)
+        plt.plot(x, y, 'bo')
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.legend(['in_Degree'])
+        plt.xlabel('$K$', fontsize=20)
+        plt.ylabel('$P_K$', fontsize=20)
+        plt.title('$Degree\,Distribution\,of\,Followers$', fontsize=20)
+        plt.show()
+degree_separation_cdf("user_followings")
+degree_separation_cdf("user_followers")
